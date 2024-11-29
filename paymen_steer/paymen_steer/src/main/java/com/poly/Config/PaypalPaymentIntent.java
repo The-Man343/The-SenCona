5773cfe2b02f17c440b2b4c7ba062b2c2c17a36d
The-Man343 <chumcf1234@gmail.com>
2024-11-29 14:05:06 +0700

    Delete paymen_steer/paymen_steer directory

--- paymen_steer/paymen_steer/src/main/java/com/poly/Config/PaypalPaymentIntent.java
+++
@@ -1,5 +0,0 @@
-package com.poly.Config;
-
-public enum PaypalPaymentIntent {
-	sale, authorize, order
-}
--- paymen_steer/paymen_steer/src/main/java/com/poly/Config/PaypalPaymentMethod.java
+++
@@ -1,5 +0,0 @@
-package com.poly.Config;
-
-public enum PaypalPaymentMethod {
-	credit_card, paypal
-}
