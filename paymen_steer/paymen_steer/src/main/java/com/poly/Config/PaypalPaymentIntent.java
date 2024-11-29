d57b9cb7a69feb96eeac41306bfba076f403fcff
The-Man343 <chumcf1234@gmail.com>
2024-11-29 14:02:19 +0700

    Add files via upload
    Hỗ trợ viết trang thông tin mạng và fix lỗi trang

---
+++ paymen_steer/paymen_steer/src/main/java/com/poly/Config/PaypalPaymentIntent.java
@@ -0,0 +1,5 @@
+package com.poly.Config;
+
+public enum PaypalPaymentIntent {
+	sale, authorize, order
+}
---
+++ paymen_steer/paymen_steer/src/test/java/com/poly/Controller/PaymenSteerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poly.Controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymenSteerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
